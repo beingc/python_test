2a54e40d9fc0191b1d762e60183fd07b06ea6f6b
beingc <xuyang.x@qq.com>
2024-07-04 21:30:18 +0800

    udpate

--- src/api_test/test_api.py
+++ src/api_test/test_api.py
@@ -1,11 +1,29 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+import random
 
 app = Flask(__name__)
 
 
+@app.route('/api/test', methods=['GET'])
+def api_test_get():
+    random_id = random.randint(100000, 999999)
+    response = {
+        "resultCode": "0",
+        "resultDesc": "Success",
+        "data": {
+            "id": random_id
+        }
+    }
+    return jsonify(response)
+
+
 @app.route('/api/test', methods=['POST'])
-def api_test():
-    return jsonify({'result': 'success'})
+def api_test_post():
+    req_data = request.get_json()
+    if 'id' in req_data:
+        return jsonify({"resultCode": "0", "resultDesc": "Success"})
+    else:
+        return jsonify({"resultCode": "-1", "resultDesc": "Failure: 'id' field is missing"}), 400
 
 
 if __name__ == '__main__':
